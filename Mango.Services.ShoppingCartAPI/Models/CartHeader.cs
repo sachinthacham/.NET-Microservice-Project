8144c1c3cd89dee9991d03ec3bedbd33c6f8f3ea
sachinthacham <sachinthachamindu26@gmail.com>
2025-06-28 17:02:17 +0530

    implement cartdetails model

---
+++ Mango.Services.ShoppingCartAPI/Models/CartHeader.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartHeader
+    {
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        [NotMapped]
+        public double Discount{ get; set; }
+        [NotMapped]
+        public double CartTotal { get; set; }
+    }
+}
